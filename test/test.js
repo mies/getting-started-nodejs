5a947b5766243d4c160ce723bb9e3481d359c875
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-05-16 13:57:33 +0200

    updated test

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@
   describe('GET', function(){
     it('respond with json', function(done){
       request(app)
-      .get('/cities.json')
+      .get('/cities.jso')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done);
